04a3b08525587adc14003ce8b7aa87f841cd8510
kikkidev <kikkidev@gmail.com>
2021-05-23 12:22:30 +0900

    Add git setting file

--- link.sh
+++ link.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.config/nvim/
 ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 exec $SHELL -l
